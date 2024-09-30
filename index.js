064fdd72abe76f9dad9d3c8cdc3be3e68c64b074
Nanards <leonardosajulga45@gmail.com>
2024-09-30 20:29:54 +0800

    first commit

--- index.js
+++ index.js
@@ -1,38 +1,60 @@
-// Import the Express library
+
 const express = require('express');
 const bodyParser = require('body-parser');
 
-// Create an instance of an Express application
+
 const app = express();
 const PORT = 3000;
 
 app.use(bodyParser.json());
 
 app.use((err, req, res, next) => {
-    console.error(err.stack); 
-    res.status(500).json({ error: 'Something went wrong!' }); 
+    console.error(err.stack);
+    res.status(500).json({ error: 'Something went wrong!' });
 });
 
-//array to store user data
+
 const users = [];
 
-// Route to handle GET requests
+
 app.get('/users', (req, res) => {
-    console.log('GET /users endpoint was accessed'); 
+    console.log('GET /users endpoint was accessed');
     res.status(200).json(users);
 });
 
+const isValidEmail = (email) => {
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    return emailRegex.test(email);
+}
 
-// Route to handle POST requests
+const isValidPassword = (password) => {
+    const passwordRegex = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[!@#$%^&*()\-+.]).{6,20}$/;
+    return passwordRegex.test(password);
+}
+
 app.post('/register', (req, res) => {
     const { name, email, password } = req.body;
+    
+    switch (true) {
+        case !name:
+            return res.status(400).json({ error: 'All Email are required.' });
+        case !email:
+            return res.status(400).json({ error: 'All Password are required.' });
+        case !password:
+            return res.status(400).json({ error: 'All fields are required.' });
+        case !isValidEmail(email):
+            return res.status(400).json({ error: 'Invalid Email format.' });
+        case !isValidPassword(password):
+            return res.status(400).json({ error: 'Invalid password format. Password must be 6-20 characters long and include uppercase, lowercase, number, and special character.' });
+        case users.some(user => user.email === email):
+            return res.status(409).json({ error: 'Email already exists.' });
+    }
+
     users.push({ name, email, password });
     console.log(`POST /users endpoint was accessed ${JSON.stringify(users)}`);
     res.status(201).json({ message: 'User registered successfully' });
 });
 
-// Start the server and listen on the specified port
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
 });
-    
